77eb49d90c1f761c27d8e834c382aaf5483dda34
sl24 <slvitalik24@gmail.com>
2021-02-28 15:36:54 +0200

    connected mongoDB and using mongoose

--- model/db.js
+++ model/db.js
@@ -1,10 +1,30 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const uriDb = process.env.URI_DB;
 
-const db = MongoClient.connect(uriDb, {
+const db = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
   useUnifiedTopology: true,
-  poolSize: 5,
+  useFindAndModify: false,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Database connection successful");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(`Database connection error: ${err.message}`);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Database disconnected");
+});
+
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  console.log("Database connection closed and app termination");
+  process.exit(1);
 });
 
 module.exports = db;
