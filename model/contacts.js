c3746e00fef20b901ab7b56663fb8527d3ee1827
sl24 <slvitalik24@gmail.com>
2021-02-28 15:46:17 +0200

    env was removed

--- model/contacts.js
+++ model/contacts.js
@@ -1,16 +1,16 @@
-const Contact = require("../service/schemas");
+const Contact = require('../service/schemas');
 
 const listContacts = async () => {
   const data = await Contact.find({});
   return data;
 };
 
-const getContactById = async (contactId) => {
+const getContactById = async contactId => {
   const data = await Contact.findOne({ _id: contactId });
   return data;
 };
 
-const addContact = async (body) => {
+const addContact = async body => {
   const data = await Contact.create(body);
   return data;
 };
@@ -19,12 +19,12 @@ const updateContact = async (contactId, body) => {
   const data = await Contact.findByIdAndUpdate(
     { _id: contactId },
     { ...body },
-    { new: true }
+    { new: true },
   );
   return data;
 };
 
-const removeContact = async (contactId) => {
+const removeContact = async contactId => {
   const data = await Contact.findByIdAndRemove({ _id: contactId });
   return data;
 };
