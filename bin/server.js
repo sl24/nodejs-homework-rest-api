b1998b03f08aa7cbd977ebdc4a299ee2ec6eab1b
sl24 <slvitalik24@gmail.com>
2021-03-01 13:56:50 +0200

    create branch 04-auth

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,6 @@
-const app = require("../app");
+const app = require('../app');
 
-const db = require("../model/db");
+const db = require('../model/db');
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,7 +8,7 @@ db.then(() => {
   app.listen(PORT, () => {
     console.log(`Server running. Use our API on port: ${PORT}`);
   });
-}).catch((err) => {
+}).catch(err => {
   console.log(`Server not running. Error message: ${err.message}`);
   process.exit(1);
 });
