31febbce1cfba7918e2f7b5230f2f35d8c8b33c3
sl24 <slvitalik24@gmail.com>
2021-03-08 21:10:30 +0200

    create branch

--- bin/server.js
+++ bin/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 db.then(() => {
   app.listen(PORT, () => {
-    console.log(`Server running. Use our API on port: ${PORT}`);
+    console.log(`Server running. Please use our API on port: ${PORT}`);
   });
 }).catch(err => {
   console.log(`Server not running. Error message: ${err.message}`);
